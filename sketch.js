38259bf035f2c2ccf953be44e939b9fa7497f0f2
Prerana-sister <83127209+Prerana-sister@users.noreply.github.com>
2021-05-31 20:59:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,70 +1,26 @@
-var catimg ;mouseimg
-function preload(){
-catimg=loadAnimation("cat1.png","Cat2.png","cat3.png","cat4.png");
-mouseimg=loadAnimation("mouse1.png","mouse2.png","mouse3.png","mouse4.png");
-gardenimg=loadImage("garden.png");
 
-
-
-
+function preload() {
+    //load the images here
 }
-
 
 function setup(){
-createCanvas(1000,800)
-
-cat=createSprite(150,160,20,50);
-cat.addAnimation("catWalking",cat_walking)
-cat.scale = 0.9;
-
-ground = createSprite(200,180,400,20);
-ground.addImage("ground",groundImage);
-ground.x = ground.width /2;
-
-mouse = createSprite(50,180,400,20);
-mouse.addImage("mouse",groundImage);
-mouse.scale
-
-
-
-
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
 
 }
 
+function draw() {
 
-
-function draw(){
-background(garden.png);
-
-
-if(cat.x-mouse.x<(cat.width-mouse.width)/2){
-
-
-}
-
-
-
-
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
 
     drawSprites();
-
 }
 
 
 function keyPressed(){
-  if (keyCode===LEFT_ARROW){
 
-    mouse.addAnimation("mouseTasing",mouse.img2);
-    mouse.addAnimation("mouseTasing");
-    mouse.frameDelay = 25;
-}
-
-    if (keyCode===LEFT_ARROW){
-
-   mouse.addAnimation("mouseTasing",mouse.img2);
-   mouse.addAnimation("mouseTasing");   
-   mouse.frameDelay = 25;
+  //For moving and changing animation write code here
 
 
-    }
 }
